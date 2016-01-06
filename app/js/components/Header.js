68e5f80d9e479c0a44df54e50230e81252c96720
858385135@qq.com <858385135@qq.com>
2016-01-06 21:12:05 +0800

    ....

--- app/js/components/Header.js
+++ app/js/components/Header.js
@@ -3,15 +3,9 @@ import React, { Component, PropTypes } from 'react'
 class Header extends Component {
 
   render() {
-    let styles = {
-      color:'red',
-      background:'#cc00cc',
-      width:"100%",
-      height:'40px'
-    };
-
     return (
-      <div style={styles}>
+      <div className="header-bar">
+        <div>1111</div>
       </div>
     )
   }
