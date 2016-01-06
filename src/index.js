79eea8eefb0afaed9c9dd48d1408abd5e082f178
858385135@qq.com <858385135@qq.com>
2016-01-06 10:48:45 +0800

    添加redux框架

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
-import React from 'react';
-import { render } from 'react-dom';
-import { App } from './App';
+import React from 'react'
+import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import App from '../containers/App'
+import configureStore from '../store/configureStore'
 
-render(<App />, document.getElementById('root'));
+const store = configureStore()
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -22,8 +22,8 @@ module.exports = {
     loaders: [{
       test: /\.jsx?/,
       loaders: ['babel'],
-      include: path.join(__dirname, 'src')
+      exclude: /node_modules/,
+      include: __dirname
     }]
-  },
-
+  }
 };
