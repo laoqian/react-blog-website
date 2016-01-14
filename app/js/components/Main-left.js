d6d85cb3472666f664ab0f4d379218f45016d90e
858385135@qq.com <858385135@qq.com>
2016-01-14 23:44:53 +0800

    级联菜单可以用了

--- app/js/components/Main-left.js
+++ app/js/components/Main-left.js
@@ -2,11 +2,22 @@ import React, { Component, PropTypes } from 'react'
 
 class MainLeft extends Component {
 
+
   render() {
     const mainMenu = this.props.mainMenu;
     return (
       <div className="main-left">
-        <ul>{mainMenu.map(menu=><li>{menu}</li>)}</ul>
+        <ul>
+          {mainMenu.Menus.map(menu=>
+            <li>{menu}
+              <ul>
+                {mainMenu.subMenu[menu].map(sub=>
+                  <li>{sub}</li>
+                )}
+              </ul>
+            </li>
+          )}
+        </ul>
       </div>
     )
   }
--- app/js/reducers/mainMenu.js
+++ app/js/reducers/mainMenu.js
@@ -1,10 +1,14 @@
 import { USER_SEARCH } from '../actions/search'
 
-const Menu = ['板块管理','主题管理','回复管理'];
+const Menus = ['板块管理','主题管理','回复管理'];
+const subMenu = {
+  "板块管理":['111','222','333'],
+  "主题管理":['444','233','366'],
+  "回复管理":['555','244','355']
+};
 
 
-
-export default function mainMenu(state = Menu, action) {
+export default function mainMenu(state = {Menus,subMenu}, action) {
   switch (action.type) {
     case USER_SEARCH:
       return action.id;
